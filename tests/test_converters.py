c444fb705818b73403e3a49bbb3e6603fa9b71fa
SebastianoF <sebastiano.ferraris@gmail.com>
2020-04-26 15:34:42 +0100

    testing and refactoring

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,11 +1,146 @@
-# from numpy import testing
+from numpy import testing
 
-# from winograd.converters import Convert
+from winograd.converters import Convert
+
+ALPHABET = [chr(i) for i in range(256)]
+WORM = 5
 
 
-# ALPHABET = [chr(i) for i in range(256)]
-# WORM = 5
+# --- Inverse consistency tests --- #
 
 
-# def test_strings_to_numbers_inverse_number_to_string():
-#     c = Convert(ALPHABET, WORM)
+def test_string2ints_ints2string():
+    c = Convert(ALPHABET, WORM)
+    input_string = "Sting & Sons,"
+    ints = c.string2ints("Sting & Sons,")
+    output_string = c.ints2string(ints)
+    testing.assert_equal(input_string, output_string)
+
+
+def test_ints2string_string2ints():
+    c = Convert(ALPHABET, WORM)
+    input_ints = [1234, 4321, 1234, 4321, 2356]
+    string = c.ints2string(input_ints)
+    output_ints = c.string2ints(string)
+    testing.assert_equal(input_ints, output_ints)
+
+
+def test_string2intsb_intsb2string():
+    c = Convert(ALPHABET, WORM)
+    input_string = "This string will become a worms of binaries"
+    worms = c.string2intsb(input_string)
+    output_string = c.intsb2string(worms)
+    testing.assert_equal(input_string, output_string)
+
+
+def test_intsb2string_string2intsb():
+    c = Convert(ALPHABET, WORM)
+    input_binary_worms = [
+        "0101010001101000011010010111001100100000",
+        "0111001101110100011100100110100101101110",
+        "0110011100100000011101110110100101101100",
+        "0110110000100000011000100110010101100011",
+        "0110111101101101011001010010000001100001",
+        "0010000001110111011011110111001001101101",
+        "0111001100100000011011110110011000100000",
+        "0110001001101001011011100110000101110010",
+        "0110100101100101011100110000000000000000",
+    ]
+    string = c.intsb2string(input_binary_worms)
+    output_intsb = c.string2intsb(string)
+    testing.assert_equal(input_binary_worms, output_intsb)
+
+
+def test_string2list_list2string():
+    c = Convert(ALPHABET, WORM)
+    input_string = "Input string"
+    output_list = c._string2list(input_string)
+    output_string = c._list2string(output_list)
+    testing.assert_equal(input_string, output_string)
+
+
+def test_list2string_string2list():
+    c = Convert(ALPHABET, WORM)
+    input_list = [73, 110, 112, 117]
+    output_string = c._list2string(input_list)
+    output_list = c._string2list(output_string)
+    testing.assert_equal(input_list, output_list)
+
+
+def test_worm2int_int2worm():
+    c = Convert(ALPHABET, WORM)
+    input_worm = [32, 12, 3, 0, 0]
+    output_int = c._worm2int(input_worm)
+    output_worm = c._int2worm(output_int)
+    testing.assert_equal(input_worm, output_worm)
+
+
+def test_int2worm_worm2int():
+    c = Convert(ALPHABET, WORM)
+    input_int = 3234
+    output_worm = c._int2worm(input_int)
+    output_int = c._worm2int(output_worm)
+    testing.assert_equal(input_int, output_int)
+
+
+# --- Numerical values tests --- #
+
+
+def test_strings_to_worms_sentence():
+    c = Convert(ALPHABET, WORM)
+    worms = c.string2ints("My funny valentine!")
+    expected_worms = [504224577869, 507350969966, 500068346977, 560295529]
+    testing.assert_equal(worms, expected_worms)
+
+
+def test_single_letter_to_worm():
+    c = Convert(ALPHABET, WORM)
+    worms = c.string2ints("A")
+    expected_worms = [65]
+    testing.assert_equal(worms, expected_worms)
+
+
+def test_single_letter_string2list():
+    c = Convert(ALPHABET, WORM)
+    output_list = c._string2list("A")
+    expected_list = [65]
+    testing.assert_equal(output_list, expected_list)
+
+
+def test_single_letter_string2list_and_padding():
+    c = Convert(ALPHABET, WORM)
+    output_list = c._zero_padding(c._string2list("A"))
+    expected_list = [65, 0, 0, 0, 0]
+    print(output_list)
+    testing.assert_equal(output_list, expected_list)
+
+
+def test_single_letter_string2list_and_padding_and_subdivide():
+    c = Convert(ALPHABET, WORM)
+    output_list = c._subdivide_list(c._zero_padding(c._string2list("A")))
+    expected_list = [[65, 0, 0, 0, 0]]
+    testing.assert_equal(output_list, expected_list)
+
+
+def test_convert_single_worm_to_num():
+    c = Convert(ALPHABET, WORM)
+    intput_worms = [[65, 0, 0, 0, 0]]
+    output_ints = c._worms2ints(intput_worms)
+    testing.assert_equal(output_ints, [65])
+
+    intput_worms = [[0, 65, 0, 0, 0]]
+    output_ints = c._worms2ints(intput_worms)
+    testing.assert_equal(output_ints, [65 * len(c.A)])
+
+    intput_worms = [[0, 0, 65, 0, 0]]
+    output_ints = c._worms2ints(intput_worms)
+    testing.assert_equal(output_ints, [65 * len(c.A) ** 2])
+
+
+# --- extreme cases tests --- #
+
+
+def test_strings_to_worm_empty_string():
+    c = Convert(ALPHABET, WORM)
+    worms = c.string2ints("")
+    testing.assert_equal(worms, [])
--- winograd/converters.py
+++ winograd/converters.py
@@ -14,55 +14,74 @@ class Convert:
             self.wl = 5
 
         self._largest_int = len(self.A) ** self.wl - 1
-        self.len_binaries = "{:b}".format(self._largest_int)
+        self.len_binaries = len("{:b}".format(self._largest_int))
 
-    def S2N(self, input_string):
+    def string2ints(self, input_string):
         """String to list of numbers of worm length w"""
         worms = self._subdivide_list(
             self._zero_padding(self._string2list(input_string))
         )
         return self._worms2ints(worms)
 
+    def ints2string(self, input_worms):
+        """From a list of integers to the corresponding word"""
+        list_nums = self._trim(self._lifter(self._ints2worms(input_worms)))
+        return self._list2string(list_nums)
+
+    def string2intsb(self, input_string):
+        """From string to list of binary worms"""
+        return self._listDec2ListBin(self.string2ints(input_string))
+
+    def intsb2string(self, input_worms_b):
+        """From worms in binary to string"""
+        return self.ints2string(self._listBin2ListDec(input_worms_b))
+
     def _string2list(self, input_string):
         """From string to corresponding letters in the alphabet"""
         return [self.A.index(letter) for letter in input_string]
 
-    def _zero_padding(self, v):
-        """Makes the input list a list of length multiple of wl, padding with zeros """
-        r = len(v) % self.wl
-        return v + [0] * ((self.wl - r) % self.wl)
-
-    def _subdivide_list(self, input_list):
-        """From a list of number to a list of lists of len wl. """
-        return [
-            input_list[j * self.wl : (j + 1) * self.wl]
-            for j in range(int(len(input_list) / self.wl))
-        ]
+    def _list2string(self, list_of_numbers):
+        """From a list of numbers to corresponding letters """
+        list_of_letters = [self.A[number] for number in list_of_numbers]
+        return "".join(list_of_letters)
 
     def _worms2ints(self, list_of_worms):
         """From a list of worms to the list of corresponding integers"""
         return [self._worm2int(w) for w in list_of_worms]
 
-    def _worm2int(self, worm):
-        return sum([w * len(self.A) ** (len(worm) - i - 1) for i, w in enumerate(worm)])
-
-    def N2S(self, v):
-        """ From a list of integers to the corresponding word"""
-        list_nums = self._trim(self._lifter(self._ints2worms(v)))
-        return self._list2string(list_nums)
-
     def _ints2worms(self, list_of_ints):
         """From list of integers to list of worms"""
         return [self._int2worm(n) for n in list_of_ints]
 
+    def _worm2int(self, worm):
+        """From a worm to the corresponding integer """
+        return sum([worm[i] * len(self.A) ** i for i in range(len(worm))])
+        # return sum([w * len(self.A) ** (len(worm) - i - 1) for i, w in enumerate(worm)])
+
     def _int2worm(self, num):
         """From integer to corresponding worm"""
         worm = [0] * self.wl
-        worm[-1] = num % len(self.A)
-        for i in range(self.wl - 2, -1, -1):
-            num = (num - worm[i + 1]) / len(self.A)
+        worm[0] = int(num % len(self.A))
+        for i in range(1, len(worm)):
+            num = (num - worm[i - 1]) / len(self.A)
             worm[i] = int(num % len(self.A))
         return worm
+
+        # worm = [0] * self.wl
+        # worm[-1] = num % len(self.A)
+        # for i in range(self.wl - 2, -1, -1):
+        #     num = (num - worm[i + 1]) / len(self.A)
+        #     worm[i] = int(num % len(self.A))
+        # return worm
+
+    def _listDec2ListBin(self, list_of_integers_base_ten):
+        return [
+            "{0:b}".format(k).zfill(self.len_binaries)
+            for k in list_of_integers_base_ten
+        ]
+
+    def _listBin2ListDec(self, list_of_base_two):
+        return [int(k, 2) for k in list_of_base_two]
 
     def _lifter(self, input_list):
         """From list of lists to single list with all their elements"""
@@ -73,25 +92,20 @@ class Convert:
             else [input_list]
         )
 
+    def _zero_padding(self, v):
+        """Makes the input list a list of length multiple of wl, padding with zeros """
+        r = len(v) % self.wl
+        return v + [0] * ((self.wl - r) % self.wl)
+
     def _trim(self, input_list):
         """Eliminates the zeros at the end of the list """
         while input_list[-1] == 0:
             input_list = input_list[:-1]
         return input_list
 
-    def _list2string(self, list_of_numbers):
-        """From a list of numbers to corresponding letters """
-        list_of_letters = [self.A[number] for number in list_of_numbers]
-        result = ""
-        for s in list_of_letters:
-            result += s
-        return result
-
-    def listTen2ListBin(self, list_of_integers_base_ten):
+    def _subdivide_list(self, input_list):
+        """From a list of number to a list of lists of len wl. """
         return [
-            "{0:b}".format(k).zfill(self.len_binaries)
-            for k in list_of_integers_base_ten
+            input_list[j * self.wl : (j + 1) * self.wl]
+            for j in range(int(len(input_list) / self.wl))
         ]
-
-    def listBin2ListDec(self, list_of_base_two):
-        return [int(k, 2) for k in list_of_base_two]
